999925efdfbb23b8dddae5dd09816a6d08b9ff63
nguyenMHgithubIT <hieunmbh01296@fpt.edu.vn>
2024-10-20 22:11:33 +0700

    Add files via upload

---
+++ ASM1/src/Student.java
@@ -0,0 +1,32 @@
+class Student {
+    private int id;
+    private String name;
+    private double marks; // Assuming marks is a double for more precision
+
+    public Student(int id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+}
